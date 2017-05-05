9b71ea232abeaf72632f05567c36c100fde28ce6
zegodev <dev@zego.im>
2017-05-05 15:56:19 +0800

    Update LiveDemo5, Version: 170505_144425_heads_master-0-g6ee2bd6-dirty

---
+++ android/LiveDemo5/app/src/main/java/com/zego/livedemo5/presenters/RoomInfo.java
@@ -0,0 +1,12 @@
+package com.zego.livedemo5.presenters;
+
+/**
+ * Copyright © 2017 Zego. All rights reserved.
+ */
+
+public class RoomInfo {
+    public String room_id;
+    public String room_name;
+    public String anchor_id_name;
+    public String anchor_nick_name;
+}
